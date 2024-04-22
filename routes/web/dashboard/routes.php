bd271f335554d59ea4d02b1d38460b7b20a5dffb
Abdelrahman Tarek <justatarek@gmail.com>
2024-04-22 08:15:26 +0200

    Fix auth routes prefix and name

--- routes/web/dashboard/routes.php
+++ routes/web/dashboard/routes.php
@@ -3,7 +3,7 @@
 use App\Http\Controllers\Dashboard\HomeController;
 use Illuminate\Support\Facades\Route;
 
-Route::prefix('dashboard')->name('dashboard.')->group(base_path(path: 'routes/web/dashboard/auth.php'));
+Route::group([], base_path(path: 'routes/web/dashboard/auth.php'));
 
 Route::middleware('auth:dashboard')->group(function () {
     Route::get('/', HomeController::class)->name('home');
