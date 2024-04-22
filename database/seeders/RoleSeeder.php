01336c339d6e1382a0777a4faa68aec70342e6b4
Abdelrahman Tarek <justatarek@gmail.com>
2024-04-22 06:56:46 +0200

    Install and configure spatie permissions

---
+++ database/seeders/RoleSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Enums\UserRole;
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
+
+class RoleSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        foreach (UserRole::cases() as $role) {
+            Role::query()->firstOrCreate(attributes: [
+                'name' => $role->value,
+            ]);
+        }
+    }
+}
