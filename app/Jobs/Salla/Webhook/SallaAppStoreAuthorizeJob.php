c4b78febf7610b0d9951774e262260aaf2f43a33
Abdelrahman Tarek <justatarek@gmail.com>
2024-05-02 09:58:31 +0300

    Fix

--- app/Jobs/Salla/Webhook/SallaAppStoreAuthorizeJob.php
+++ app/Jobs/Salla/Webhook/SallaAppStoreAuthorizeJob.php
@@ -10,6 +10,7 @@ use App\Models\Store;
 use App\Models\User;
 use App\Notifications\Salla\UserCreatedUsingSallaWebhook;
 use App\Services\Salla\OAuth\SallaOAuthService;
+use Exception;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
@@ -42,62 +43,65 @@ class SallaAppStoreAuthorizeJob implements ShouldQueue
             return;
         }
 
-        $resourceOwner = (new SallaOAuthService())->getResourceOwner(accessToken: $this->data['access_token']);
-        $email = $resourceOwner->getEmail();
+        try {
+            $resourceOwner = (new SallaOAuthService())->getResourceOwner(accessToken: $this->data['access_token']);
+            $email = $resourceOwner->getEmail();
 
-        $user = User::where(column: 'email', operator: '=', value: $email)->first();
-        if ($user === null) {
-            $userData = DB::transaction(callback: function () use ($resourceOwner, $email): array {
-                $password = Str::password();
+            $user = User::where(column: 'email', operator: '=', value: $email)->first();
+            if ($user === null) {
+                $userData = DB::transaction(callback: function () use ($resourceOwner, $email): array {
+                    $password = Str::password();
 
-                $user = User::query()->create(attributes: [
-                    'name' => $resourceOwner->getName(),
-                    'email' => $email,
-                    'password' => $password,
+                    $user = User::query()->create(attributes: [
+                        'name' => $resourceOwner->getName(),
+                        'email' => $email,
+                        'password' => $password,
+                    ]);
+
+                    $user->assignRole(UserRole::MERCHANT);
+
+                    $user->widget()->create();
+
+                    return [
+                        'user' => $user,
+                        'password' => $password,
+                    ];
+                });
+
+                $user = $userData['user'];
+
+                $user->notify(instance: new UserCreatedUsingSallaWebhook(
+                    email: $email,
+                    password: $userData['password'],
+                ));
+            }
+
+            DB::transaction(callback: function () use ($resourceOwner, $user): void {
+                $user->tokens()->create(attributes: [
+                    'provider_type' => ProviderType::SALLA,
+                    'access_token' => $this->data['access_token'],
+                    'refresh_token' => $this->data['refresh_token'],
+                    'expired_at' => $this->data['expires'],
                 ]);
 
-                $user->assignRole(UserRole::MERCHANT);
+                $data = $resourceOwner->toArray();
 
-                $user->widget()->create();
-
-                return [
-                    'user' => $user,
-                    'password' => $password,
-                ];
+                $user->store()->create(attributes: [
+                    'provider_type' => ProviderType::SALLA,
+                    'provider_id' => $data['merchant']['id'],
+                    'name' => $data['merchant']['name'],
+                    'mobile' => $data['mobile'],
+                    'email' => $data['email'],
+                    'domain' => $data['merchant']['domain'],
+                ]);
             });
 
-            $user = $userData['user'];
-
-            $user->notify(instance: new UserCreatedUsingSallaWebhook(
-                email: $email,
-                password: $userData['password'],
-            ));
+            Bus::chain(jobs: [
+                Bus::batch(jobs: new SallaPullCustomersJob(accessToken: $this->data['access_token'], userId: $user->id))->name(name: 'salla.pull.customers:'.$user->id),
+                Bus::batch(jobs: new SallaPullAbandonedCartsJob(accessToken: $this->data['access_token'], userId: $user->id))->name(name: 'salla.pull.abandoned-carts:'.$user->id),
+            ])->dispatch();
+        } catch (Exception $e) {
+            logger()->error(message: $e);
         }
-
-        DB::transaction(callback: function () use ($resourceOwner, $user): void {
-            $user->tokens()->create(attributes: [
-                'provider_type' => ProviderType::SALLA,
-                'access_token' => $this->data['access_token'],
-                'refresh_token' => $this->data['refresh_token'],
-                'expired_at' => $this->data['expires'],
-            ]);
-
-            $data = $resourceOwner->toArray();
-
-            $user->store()->create(attributes: [
-                'provider_type' => ProviderType::SALLA,
-                'provider_id' => $data['merchant']['id'],
-                'name' => $data['merchant']['name'],
-                'mobile' => $data['mobile'],
-                'email' => $data['email'],
-                'domain' => $data['merchant']['domain'],
-            ]);
-        });
-
-        Bus::chain(jobs: [
-            Bus::batch(jobs: new SallaPullCustomersJob(accessToken: $this->data['access_token'], userId: $user->id))->name(name: 'salla.pull.customers:'.$user->id),
-            Bus::batch(jobs: new SallaPullAbandonedCartsJob(accessToken: $this->data['access_token'], userId: $user->id))->name(name: 'salla.pull.abandoned-carts:'.$user->id),
-        ])->dispatch();
-
     }
 }
