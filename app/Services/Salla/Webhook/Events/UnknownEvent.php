0ac7d3f023e9f4c2d5a93e0682b652a2c028f005
Abdelrahman Tarek <justatarek@gmail.com>
2024-05-28 18:45:08 +0300

    Update salla unknown event

--- app/Services/Salla/Webhook/Events/UnknownEvent.php
+++ app/Services/Salla/Webhook/Events/UnknownEvent.php
@@ -8,11 +8,28 @@ class UnknownEvent implements SallaWebhookEvent
 {
     public function __invoke(string $event, int $merchantId, array $data): void
     {
+        $excluded = [
+            'app.installed',
+        ];
+
+        if (in_array(needle: $event, haystack: $excluded)) {
+            return;
+        }
+
         $json = json_encode(
             value: $data,
             flags: JSON_UNESCAPED_UNICODE,
         );
 
-        logger()->warning(message: 'Salla unknown webhook event'.PHP_EOL."Event: {$event}".PHP_EOL."Merchant: {$merchantId}".PHP_EOL.$json);
+        logger()->warning(
+            message: generateMessageUsingSeparatedLines(
+                lines: [
+                    'Salla unknown webhook event',
+                    "Event: {$event}",
+                    "Merchant: {$merchantId}",
+                    $json,
+                ],
+            ),
+        );
     }
 }
