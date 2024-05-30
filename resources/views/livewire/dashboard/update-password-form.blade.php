aa1ce91a83392dbcc719cc9150b7a68b580e4b3e
Abdelrahman Tarek <justatarek@gmail.com>
2024-05-30 14:13:16 +0300

    Fix update password form style

--- resources/views/livewire/dashboard/update-password-form.blade.php
+++ resources/views/livewire/dashboard/update-password-form.blade.php
@@ -1,45 +1,47 @@
 <div class="row">
-    <div class="card">
-        <div class="card-header">
-            <h3 class="card-title">
-                @lang('dashboard.pages.settings.index.password.title')
-            </h3>
-        </div>
-        <div class="card-body">
-            <div class="form-group mb-3">
-                <label class="form-label" for="name">
-                    @lang('dashboard.pages.settings.index.password.current_password')
-                </label>
-                <input type="password" @class(['form-control', 'is-invalid' => $errors->has(key: 'currentPassword')]) wire:model="currentPassword">
-                @error('currentPassword')
-                <div class="invalid-feedback">
-                    {{ $message }}
+    <div class="col-12">
+        <div class="card">
+            <div class="card-header">
+                <h3 class="card-title">
+                    @lang('dashboard.pages.settings.index.password.title')
+                </h3>
+            </div>
+            <div class="card-body">
+                <div class="form-group mb-3">
+                    <label class="form-label" for="name">
+                        @lang('dashboard.pages.settings.index.password.current_password')
+                    </label>
+                    <input type="password" @class(['form-control', 'is-invalid' => $errors->has(key: 'currentPassword')]) wire:model="currentPassword">
+                    @error('currentPassword')
+                    <div class="invalid-feedback">
+                        {{ $message }}
+                    </div>
+                    @enderror
                 </div>
-                @enderror
-            </div>
-            <div class="form-group mb-3">
-                <label class="form-label" for="name">
-                    @lang('dashboard.pages.settings.index.password.new_password')
-                </label>
-                <input type="password" @class(['form-control', 'is-invalid' => $errors->has(key: 'newPassword')]) wire:model="newPassword">
-                @error('newPassword')
-                <div class="invalid-feedback">
-                    {{ $message }}
+                <div class="form-group mb-3">
+                    <label class="form-label" for="name">
+                        @lang('dashboard.pages.settings.index.password.new_password')
+                    </label>
+                    <input type="password" @class(['form-control', 'is-invalid' => $errors->has(key: 'newPassword')]) wire:model="newPassword">
+                    @error('newPassword')
+                    <div class="invalid-feedback">
+                        {{ $message }}
+                    </div>
+                    @enderror
                 </div>
-                @enderror
+                <div class="form-group">
+                    <label class="form-label" for="name">
+                        @lang('dashboard.pages.settings.index.password.new_password_confirmation')
+                    </label>
+                    <input type="password" class="form-control" wire:model="newPasswordConfirmation">
+                </div>
             </div>
-            <div class="form-group">
-                <label class="form-label" for="name">
-                    @lang('dashboard.pages.settings.index.password.new_password_confirmation')
-                </label>
-                <input type="password" class="form-control" wire:model="newPasswordConfirmation">
+            <div class="card-footer">
+                <button class="btn btn-warning" wire:click="updatePassword" wire:loading.attr="disabled">
+                    @lang('dashboard.common.edit')
+                    @lang('dashboard.pages.settings.index.password.title')
+                </button>
             </div>
-        </div>
-        <div class="card-footer">
-            <button class="btn btn-warning" wire:click="updatePassword" wire:loading.attr="disabled">
-                @lang('dashboard.common.edit')
-                @lang('dashboard.pages.settings.index.password.title')
-            </button>
         </div>
     </div>
 </div>
