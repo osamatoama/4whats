1ccd6be30ab88d1f6fda7057ffac1e49e570ff13
Abdelrahman Tarek <justatarek@gmail.com>
2024-06-02 12:40:46 +0300

    Fix whatsapp connection bar style

--- resources/views/livewire/dashboard/whatsapp-connection/bar.blade.php
+++ resources/views/livewire/dashboard/whatsapp-connection/bar.blade.php
@@ -6,32 +6,40 @@
             @lang('dashboard.whatsapp.disconnected')
         @endif
     </div>
-    <div>
+    <div class="d-flex flex-column flex-md-row gap-1">
         @if($this->isConnected)
             @if($this->isDisconnecting)
-                <button class="btn btn-sm btn-danger" wire:poll.keep-alive.5s disabled>
-                    @lang('dashboard.whatsapp.disconnecting')
-                </button>
+                <div>
+                    <button class="btn btn-sm btn-danger" wire:poll.keep-alive.5s disabled>
+                        @lang('dashboard.whatsapp.disconnecting')
+                    </button>
+                </div>
             @else
                 @can('disconnect', $this->whatsappAccount)
-                    <button class="btn btn-sm btn-danger" wire:confirm="@lang('dashboard.common.are_you_sure')" wire:click="disconnect" wire:loading.attr="disabled">
-                        @lang('dashboard.whatsapp.disconnect')
-                    </button>
+                    <div>
+                        <button class="btn btn-sm btn-danger" wire:confirm="@lang('dashboard.common.are_you_sure')" wire:click="disconnect" wire:loading.attr="disabled">
+                            @lang('dashboard.whatsapp.disconnect')
+                        </button>
+                    </div>
                 @endcan
 
                 @can('disableSending', $this->whatsappAccount)
                     @if($this->whatsappAccount->is_sending_enabled)
-                        <button class="btn btn-sm btn-danger" wire:confirm="@lang('dashboard.common.are_you_sure')" wire:click="disableSending" wire:loading.attr="disabled">
-                            @lang('dashboard.whatsapp.disable_sending')
-                        </button>
+                        <div>
+                            <button class="btn btn-sm btn-danger" wire:confirm="@lang('dashboard.common.are_you_sure')" wire:click="disableSending" wire:loading.attr="disabled">
+                                @lang('dashboard.whatsapp.disable_sending')
+                            </button>
+                        </div>
                     @endif
                 @endcan
 
                 @can('enableSending', $this->whatsappAccount)
                     @if($this->whatsappAccount->is_sending_disabled)
-                        <button class="btn btn-sm btn-success" wire:confirm="@lang('dashboard.common.are_you_sure')" wire:click="enableSending" wire:loading.attr="disabled">
-                            @lang('dashboard.whatsapp.enable_sending')
-                        </button>
+                        <div>
+                            <button class="btn btn-sm btn-success" wire:confirm="@lang('dashboard.common.are_you_sure')" wire:click="enableSending" wire:loading.attr="disabled">
+                                @lang('dashboard.whatsapp.enable_sending')
+                            </button>
+                        </div>
                     @endif
                 @endcan
             @endif
