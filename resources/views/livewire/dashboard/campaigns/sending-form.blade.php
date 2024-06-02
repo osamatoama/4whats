40e310fc83c13a2b98bc0c0396b50f985d8ff55c
Ahmed Saber <ahmed0saber33@gmail.com>
2024-06-02 19:06:47 +0300

    Fix select input in `sending-form`

--- resources/views/livewire/dashboard/campaigns/sending-form.blade.php
+++ resources/views/livewire/dashboard/campaigns/sending-form.blade.php
@@ -8,13 +8,32 @@
                     <label class="form-label">
                         @lang('dashboard.pages.campaigns.columns.type.label')
                     </label>
-                    <select @class(['form-select', 'is-invalid' => $errors->has(key: 'currentType')]) wire:model.live="currentType">
+                    <select @class(['d-none form-select hidden-select', 'is-invalid' => $errors->has(key: 'currentType')]) wire:model.live="currentType">
                         @foreach($types as $type)
                             <option value="{{ $type->value }}" wire:key="{{ $type->value }}">
                                 {{ $type->label() }}
                             </option>
                         @endforeach
                     </select>
+                    <div class="dropdown">
+                        <button style="color:#000444" class="btn dropdown-toggle custom-select w-100 border d-flex justify-content-between align-items-center" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            <span>
+                                أختر النوع
+                            </span>
+                            <svg width="20" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor">
+                                <path fill-rule="evenodd" d="M4.22 6.22a.75.75 0 0 1 1.06 0L8 8.94l2.72-2.72a.75.75 0 1 1 1.06 1.06l-3.25 3.25a.75.75 0 0 1-1.06 0L4.22 7.28a.75.75 0 0 1 0-1.06Z" clip-rule="evenodd" />
+                            </svg>
+                        </button>
+                        <ul class="dropdown-menu w-100">
+                            @foreach($types as $type)
+                                <li>
+                                    <button class="dropdown-item custom-option" value="{{ $type->value }}" wire:key="{{ $type->value }}">
+                                        {{ $type->label() }}
+                                    </button>
+                                </li>
+                            @endforeach
+                        </ul>
+                    </div>
                     @error('currentType')
                     <div class="invalid-feedback">{{ $message }}</div>
                     @enderror
@@ -42,3 +61,16 @@
         </div>
     </div>
 </div>
+<script>
+    const hiddenSelect = document.querySelector(".hidden-select")
+    const customOptions = document.querySelectorAll(".custom-option")
+    const customSelect = document.querySelector(".custom-select")
+    customOptions.forEach(option => {
+        option.addEventListener("click", () => {
+            const changeEvent = new Event('change')
+            hiddenSelect.querySelector(`option[value=${option.value}]`).selected = "selected"
+            hiddenSelect.dispatchEvent(changeEvent)
+            customSelect.querySelector("span").textContent = option.textContent
+        })
+    })
+</script>
