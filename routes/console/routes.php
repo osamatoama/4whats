54bd875022f05e20794598f3fb0795997c07cd22
Abdelrahman Tarek <justatarek@gmail.com>
2024-08-05 01:20:40 +0300

    Update routes.php

--- routes/console/routes.php
+++ routes/console/routes.php
@@ -1,5 +1,7 @@
 <?php
 
+use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schedule;
 
 Schedule::command(
@@ -15,7 +17,23 @@ Schedule::command(
     time: '01:30',
 );
 
-Schedule::command(
-    command: 'queue:retry',
-    parameters: ['all'],
-)->everyTenMinutes();
+Artisan::command(
+    signature: 'queue:retry-too-many-requests-failed-jobs',
+    callback: function (): void {
+        DB::table(
+            table: config(
+                key: 'queue.failed.table',
+            ),
+        )->where(
+            column: 'exception',
+            operator: 'LIKE',
+            value: '%Too Many Requests%',
+        )->get()->each(
+            callback: function (object $failedJob) {
+                Artisan::call(
+                    command: "queue:retry {$failedJob->uuid}",
+                );
+            },
+        );
+    },
+)->everyMinute();
