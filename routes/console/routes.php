81e9637367f047cc93db354e58510c25280b0808
Mohamed Alaa <mohamedalaa2104@gmail.com>
2025-01-03 15:33:37 +0200

    Remoev test jobs

--- routes/console/routes.php
+++ routes/console/routes.php
@@ -4,50 +4,6 @@ use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schedule;
 
-//Schedule::command(
-//    command: 'queue:work --stop-when-empty',
-//)
-//    ->everyMinute()
-//    ->withoutOverlapping();
-//
-//Schedule::command(
-//    command: 'queue:work --queue=subscriptions --stop-when-empty',
-//)
-//    ->everyMinute()
-//    ->withoutOverlapping();
-//
-//Schedule::command(
-//    command: 'queue:work --queue=orders --stop-when-empty',
-//)
-//    ->everyMinute()
-//    ->withoutOverlapping();
-//
-//Schedule::command(
-//    command: 'queue:work --queue=customers --stop-when-empty',
-//)
-//    ->everyMinute()
-//    ->withoutOverlapping();
-//
-//Schedule::command(
-//    command: 'queue:work --queue=abandoned-carts --stop-when-empty',
-//)
-//    ->everyMinute()
-//    ->withoutOverlapping();
-//
-//Schedule::command(
-//    command: 'queue:restart',
-//)
-//    ->everyFiveMinutes();
-
-Schedule::call(function (): void {
-    \App\Jobs\TestJob::dispatch();
-    \App\Jobs\TestJob::dispatch()->onQueue('subscriptions');
-    \App\Jobs\TestJob::dispatch()->onQueue('orders');
-    \App\Jobs\TestJob::dispatch()->onQueue('customers');
-    \App\Jobs\TestJob::dispatch()->onQueue('abandoned-carts');
-    \App\Jobs\TestJob::dispatch()->onQueue('others');
-})->everyMinute();
-
 Schedule::command(
     command: 'backup:clean',
 )->dailyAt(
