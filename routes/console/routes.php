6c8e1e3659784966c9313ddb6ee4ac7b2b8522b4
Mohamed Alaa <mohamedalaa2104@gmail.com>
2025-01-03 14:00:06 +0200

    Stop queue from console

--- routes/console/routes.php
+++ routes/console/routes.php
@@ -4,40 +4,40 @@ use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schedule;
 
-Schedule::command(
-    command: 'queue:work --stop-when-empty',
-)
-    ->everyMinute()
-    ->withoutOverlapping();
-
-Schedule::command(
-    command: 'queue:work --queue=subscriptions --stop-when-empty',
-)
-    ->everyMinute()
-    ->withoutOverlapping();
-
-Schedule::command(
-    command: 'queue:work --queue=orders --stop-when-empty',
-)
-    ->everyMinute()
-    ->withoutOverlapping();
-
-Schedule::command(
-    command: 'queue:work --queue=customers --stop-when-empty',
-)
-    ->everyMinute()
-    ->withoutOverlapping();
-
-Schedule::command(
-    command: 'queue:work --queue=abandoned-carts --stop-when-empty',
-)
-    ->everyMinute()
-    ->withoutOverlapping();
-
-Schedule::command(
-    command: 'queue:restart',
-)
-    ->everyFiveMinutes();
+//Schedule::command(
+//    command: 'queue:work --stop-when-empty',
+//)
+//    ->everyMinute()
+//    ->withoutOverlapping();
+//
+//Schedule::command(
+//    command: 'queue:work --queue=subscriptions --stop-when-empty',
+//)
+//    ->everyMinute()
+//    ->withoutOverlapping();
+//
+//Schedule::command(
+//    command: 'queue:work --queue=orders --stop-when-empty',
+//)
+//    ->everyMinute()
+//    ->withoutOverlapping();
+//
+//Schedule::command(
+//    command: 'queue:work --queue=customers --stop-when-empty',
+//)
+//    ->everyMinute()
+//    ->withoutOverlapping();
+//
+//Schedule::command(
+//    command: 'queue:work --queue=abandoned-carts --stop-when-empty',
+//)
+//    ->everyMinute()
+//    ->withoutOverlapping();
+//
+//Schedule::command(
+//    command: 'queue:restart',
+//)
+//    ->everyFiveMinutes();
 
 Schedule::command(
     command: 'backup:clean',
