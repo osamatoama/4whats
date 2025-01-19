4cf02109d72b2cbeb238fd17612f1735aa89eee9
Mohamed Alaa <mohamedalaa2104@gmail.com>
2025-01-19 23:47:51 +0200

    remove urlencode from redirect_uri

--- app/Services/Zid/OAuth/ZidOAuthService.php
+++ app/Services/Zid/OAuth/ZidOAuthService.php
@@ -17,9 +17,9 @@ final readonly class ZidOAuthService
                 'client_id' => config(
                     key: 'services.zid.client_id',
                 ),
-                'redirect_uri' => urlencode(route(
+                'redirect_uri' => route(
                     name: 'dashboard.oauth.zid.callback',
-                )).'?',
+                ),
                 'response_type' => 'code',
             ],
         );
